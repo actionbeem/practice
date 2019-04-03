d6bca2e0003a64b827bd5ee95191b1033021db68
김종혁 <actionbeem929@gmail.com>
2019-04-03 11:59:58 +0900

    글 삭제 구현

--- node/main.js
+++ node/main.js
@@ -54,7 +54,14 @@ var app = http.createServer(function(request,response){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`)
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>
+             <a href="/update?id=${title}">update</a>
+             <form action="delete_process" method="post" onsubmit="">
+               <input type="hidden" name="id" value="${title}">
+               <input type="submit" value="delete">
+             </form>
+            `)
             response.writeHead(200);
             response.end(template);
           })
@@ -124,9 +131,22 @@ var app = http.createServer(function(request,response){
         console.log(post)
         fs.rename(`data/${id}`, `data/${title}`, function(error){
           fs.writeFile(`data/${title}`, description , 'utf8', function(err){
-            response.writeHead(200);
-            response.end('success');          
+            response.writeHead(302, { Location: `/?id=${title}` });
+            response.end();          
           });
+        })
+      })      
+    } else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      })
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, function(error){
+          response.writeHead(302, { Location: `/` });
+          response.end();    
         })
       })      
     } else {
