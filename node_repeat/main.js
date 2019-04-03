a1265395ed49609aee2987ca46b19475d4b91a84
김종혁 <actionbeem929@gmail.com>
2019-04-03 18:31:44 +0900

    data 폴더 파일 읽기

--- node_repeat/main.js
+++ node_repeat/main.js
@@ -9,16 +9,20 @@ let app = http.createServer((request, response) => {
 
   if(pathname === '/'){
     if(queryData.id === undefined){
-      let title = 'Welcome';
-      let description = 'hello world'
+      fs.readdir(`data/`, (err, filelist) => {
+        let title = 'Welcome';
+        let description = 'hello world'
 
-      response.writeHead(200);
-      response.end(html(title, description));
-    } else {
-      fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
-        let title = queryData.id;
         response.writeHead(200);
-        response.end(html(title, description));
+        response.end(template.html(title, description, filelist));
+      })
+    } else {
+      fs.readdir(`data/`, (err, filelist) => { 
+        fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
+          let title = queryData.id;
+          response.writeHead(200);
+          response.end(template.html(title, description, filelist));
+        })
       })
     }
   } else {
@@ -28,28 +32,34 @@ let app = http.createServer((request, response) => {
 
 });
 
-let html = function(title, description){
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>Node_repeat</title>
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <div>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </div>
-    </body>
-    </html>
-  `
+let template = {
+  list(filelist){
+    let body = '<ul>'
+    filelist.forEach(file => {
+      body += `<li><a href="/?id=${file}">${file}</a></li>`
+    })
+    body += '</ul>'
+    return body;
+  },
+  html(title, description, filelist){
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Node_repeat</title>
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        ${this.list(filelist)}
+        <div>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </div>
+      </body>
+      </html>
+    `
+  },
 }
 
 app.listen(3000)
