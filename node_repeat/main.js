57b15e5ddbdefbb97fd340c5a2c199767fc65d81
김종혁 <actionbeem929@gmail.com>
2019-04-04 11:21:18 +0900

    update 구현

--- node_repeat/main.js
+++ node_repeat/main.js
@@ -1,6 +1,7 @@
 let http = require('http');
 let fs = require('fs');
 let url = require('url');
+let qs = require('querystring')
 
 let app = http.createServer((request, response) => {
   let _url = request.url;
@@ -21,7 +22,7 @@ let app = http.createServer((request, response) => {
       fs.readdir(`data/`, (err, filelist) => { 
         fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
           let title = queryData.id;
-          let control = `<a href="/create">create</a><a href="/update">update</a>`
+          let control = `<a href="/create">create</a><a href="/update?id=${title}">update</a>`
           response.writeHead(200);
           response.end(template.html(title, description, filelist, control));
         })
@@ -33,7 +34,7 @@ let app = http.createServer((request, response) => {
       let form = `
         <form action="/create_process" method="post">
           <input type="text" name="title">
-          <input type="text" name="description">
+          <textarea name="description"></textarea>
           <input type="submit">
         </form>
       `
@@ -44,7 +45,55 @@ let app = http.createServer((request, response) => {
     })
 
   } else if (pathname === '/create_process'){
+    let body = '';
+    request.on('data', data => {
+      body += data;
+    })
+    request.on('end', () => {
+      let post = qs.parse(body);
+      let title = post.title;
+      let description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', err => {
+        response.writeHead(302, { Location: `/`});
+        response.end();
+      })
+    })
+  } else if (pathname === '/update'){
+    fs.readdir(`data/`, (err, filelist) => {
+      let title = queryData.id;
+      fs.readFile(`data/${title}`, 'utf8', (err, description) => {
+        let form = `
+          <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <input type="text" name="title" value="${title}">
+            <textarea name="description">${description}</textarea>
+            <input type="submit">
+          </form>
+        `
+        let control = `<a href="/create">create</a><a href="/update?id=${title}">update</a>`
 
+        response.writeHead(200)
+        response.end(template.html(title, form, filelist, control))
+      })
+
+    })    
+  } else if (pathname === '/update_process'){
+    let body = '';
+    request.on('data', data => {
+      body += data;
+    })
+    request.on('end', () => {
+      let post = qs.parse(body);
+      let id = post.id; 
+      let title = post.title;
+      let description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, err => {
+        fs.writeFile(`data/${title}`, description, 'utf8', err => {
+          response.writeHead(302, { Location: `/?id=${title}`})
+          response.end()
+        })
+      })
+    })
   } else {
     response.writeHead(404);
     response.end('not found');
