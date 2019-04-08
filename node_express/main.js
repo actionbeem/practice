378004423008cea0bcd8b4144f380ae4759db1c0
김종혁 <actionbeem929@gmail.com>
2019-04-08 18:05:26 +0900

    수정 기능 구현

--- node_express/main.js
+++ node_express/main.js
@@ -33,7 +33,7 @@ app.get('/page/:pageId', function (request, response) {
       var html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
-          <a href="/update?id=${sanitizedTitle}">update</a>
+          <a href="/update/${sanitizedTitle}">update</a>
           <form action="delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
@@ -79,11 +79,11 @@ app.post('/create_process', function(request, response){
   });
 });
 
-app.get('/update', function(request, response){
+app.get('/update/:pageId', function(request, response){
   fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(queryData.id).base;
+    var filteredId = path.parse(request.params.pageId).base;
     fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = queryData.id;
+      var title = request.params.pageId;
       var list = template.list(filelist);
       var html = template.HTML(title, list,
         `
@@ -100,9 +100,27 @@ app.get('/update', function(request, response){
         `,
         `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
       );
-      response.writeHead(200);
-      response.end(html);
+      response.send(html);
     });
+  });
+});
+
+app.post('/update_process', function(request, response){
+  var body = '';
+  request.on('data', function(data){
+      body = body + data;
+  });
+  request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function(error){
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+        })
+      });
   });
 });
 
@@ -159,22 +177,7 @@ var app = http.createServer(function(request,response){
         });
       });
     } else if(pathname === '/update_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-            })
-          });
-      });
+      
     } else if(pathname === '/delete_process'){
       var body = '';
       request.on('data', function(data){
