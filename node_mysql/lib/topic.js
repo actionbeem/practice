6c84bbcb25e480ea71a28b3b19b9313b9a6a34b7
김종혁 <actionbeem929@gmail.com>
2019-04-08 16:27:41 +0900

    sanitize

--- node_mysql/lib/topic.js
+++ node_mysql/lib/topic.js
@@ -2,6 +2,7 @@ var db = require('./db.js')
 var template = require('./template.js');
 var url = require('url');
 var qs = require('querystring');
+var sanitizeHtml = require('sanitize-html')
 
 exports.home = function(request, response){
   db.query(`SELECT * FROM topic`, (error, topics) => {
@@ -31,9 +32,9 @@ exports.page = function(request, response){
       var description = topic[0].description;
       var list = template.list(topics);
       var html = template.HTML(title, list, 
-        `<h2>${title}</h2>
-         ${description}
-         by ${topic[0].name}
+        `<h2>${sanitizeHtml(title)}</h2>
+         ${sanitizeHtml(description)}
+         by ${sanitizeHtml(topic[0].name)}
         `,
        `<a href="/create">create</a>
         <a href="/update?id=${queryData.id}">update</a>
@@ -52,7 +53,7 @@ exports.create = function(request, response){
     db.query(`SELECT * FROM author`, (error2, authors) => {
       var title = 'Create';
       var list = template.list(topics);
-      var html = template.HTML(title, list, `
+      var html = template.HTML(sanitizeHtml(title), list, `
       <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
@@ -106,13 +107,13 @@ exports.update = function(request, response){
       }
       db.query(`SELECT * FROM author`, (error2, authors) => {
         var list = template.list(topics);
-        var html = template.HTML(topic[0].title, list,
+        var html = template.HTML(sanitizeHtml(topic[0].title), list,
           `
           <form action="/update_process" method="post">
             <input type="hidden" name="id" value="${topic[0].id}">
-            <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+            <p><input type="text" name="title" placeholder="title" value="${sanitizeHtml(topic[0].title)}"></p>
             <p>
-              <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+              <textarea name="description" placeholder="description">${sanitizeHtml(topic[0].description)}</textarea>
             </p>
             <p>
               ${template.authorSelect(authors, topic[0].author_id)}
