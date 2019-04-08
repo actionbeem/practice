5aab9abcabafa6fa7f18613893ca330d0fa72ec2
김종혁 <actionbeem929@gmail.com>
2019-04-08 17:34:20 +0900

    home 구현

--- node_express/main.js
+++ node_express/main.js
@@ -1,3 +1,27 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var template = require('./lib/template.js');
+
+
+app.get('/', function (request, response) {
+  fs.readdir('./data', function(error, filelist){
+    var title = 'Welcome';
+    var description = 'Hello, Node.js -,.-';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    );
+    response.send(html);
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+/*
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -14,7 +38,7 @@ var app = http.createServer(function(request,response){
       if(queryData.id === undefined){
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
-          var description = 'Hello, Node.js';
+          var description = 'Hello, Node.js -,.-';
           var list = template.list(filelist);
           var html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
@@ -141,3 +165,4 @@ var app = http.createServer(function(request,response){
     }
 });
 app.listen(3000);
+*/
