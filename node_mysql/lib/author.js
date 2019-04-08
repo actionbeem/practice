b591958bbf641e8d46c3e1ce6a76825c44715d68
김종혁 <actionbeem929@gmail.com>
2019-04-08 15:13:18 +0900

    author - create 구현

--- node_mysql/lib/author.js
+++ node_mysql/lib/author.js
@@ -1,5 +1,6 @@
 var db = require('./db.js')
 var template = require('./template.js');
+var qs = require('querystring');
 
 exports.home = function(request, response){
   db.query(`SELECT * FROM topic`, (error, topics) => {
@@ -13,10 +14,42 @@ exports.home = function(request, response){
         table { border-collapse: collapse; }
         td { border:1px solid #000; }  
         </style>
+        <form action="/author/create_process" method="post">
+          <p>
+            <input type="text" name="name" placeholder="name">
+          </p>
+          <p>
+            <textarea name="profile" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
       `,
-      `<a href="/create">create</a>` )
+      `` )
       response.writeHead(200);
       response.end(html);
     });
+  });
+}
+
+exports.create_process = function(request, response){
+  var body = '';
+  request.on('data', function(data){
+      body = body + data;
+  });
+  request.on('end', function(){
+      var post = qs.parse(body);
+      db.query(`
+        INSERT INTO author (name, profile) VALUES(?,?)`,
+          [post.name, post.profile],
+          function(err, result){
+            if(err){
+              throw err
+            }
+            response.writeHead(302, { Location: `/author`} )
+            response.end();
+            }
+        )
   });
 }
