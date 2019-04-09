4c06755aa8b0cbe719a93016e5784753cc8ab628
김종혁 <actionbeem929@gmail.com>
2019-04-09 13:39:53 +0900

    라우팅 - 파일로 분리 2

--- node_express/main.js
+++ node_express/main.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var template = require('./lib/template.js');
-var qs = require('querystring');
 var bodyParser = require('body-parser');
 
-
 var compression = require('compression');
+var indexRouter = require('./routes/index')
 var topicRouter = require('./routes/topic')
 
 app.use(express.static('public'));
@@ -19,21 +17,8 @@ app.get('*',function(request, response, next){
   });
 })
 
-app.get('/', function (request, response) {
-  console.log(request.list)
-  var title = 'Welcome';
-  var description = 'Hello, Node.js -,.-';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list,
-    `<h2>${title}</h2>${description}
-     <img src="/images/sample.jpg" style="width:300px; display:block; margin-top:10px;">
-    `,
-    `<a href="/topic/create">create</a>`
-  );
-  response.send(html);
 
-});
-
+app.use('/', indexRouter);
 app.use('/topic', topicRouter);
 
 app.use(function(req, res, next) {
