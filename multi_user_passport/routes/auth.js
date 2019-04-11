aca4967e23619abc94de10869b536551ee7427a2
김종혁 <actionbeem929@gmail.com>
2019-04-11 13:36:07 +0900

    회원정보 저장 & 세션 스토어 저장

--- multi_user_passport/routes/auth.js
+++ multi_user_passport/routes/auth.js
@@ -4,6 +4,8 @@ var path = require('path');
 var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
+var shortid = require('shortid')
+var db = require('../lib/db')
 
 module.exports = function(passport){
   router.get('/login', function(request, response){
@@ -51,14 +53,39 @@ module.exports = function(passport){
     var html = template.HTML(title, list, `
       <div style="color:red">${feedback}</div>
       <form action="/auth/register_process" method="post">
-        <p><input type="text" name="email" placeholder="email"></p>
-        <p><input type="password" name="pwd" placeholder="password"></p>
-        <p><input type="password" name="pwd2" placeholder="password"></p>
-        <p><input type="text" name="displayName" placeholder="display name"></p>
+        <p><input type="text" name="email" placeholder="email" value="test01@gmail.com"></p>
+        <p><input type="password" name="pwd" placeholder="password" value="1111"></p>
+        <p><input type="password" name="pwd2" placeholder="password" value="1111"></p>
+        <p><input type="text" name="displayName" placeholder="display name" value="tester01"></p>
         <p><input type="submit" value="register"></p>
       </form>
     `, '');
     response.send(html);
+  });
+
+  router.post('/register_process', function(request, response){
+    var post = request.body;
+    var email = post.email;
+    var pwd = post.pwd;
+    var pwd2 = post.pwd2;
+    var displayName = post.displayName;
+    if(pwd !== pwd2){
+      request.flash('error', 'Password must same!!');
+      response.redirect('/auth/register')
+    } else {
+      var user = {
+        id:shortid.generate(),
+        email:email,
+        password:pwd,
+        displayName:displayName
+      }
+      db.get('users').push(user).write();
+      request.login(user, function(err){
+        console.log('redirect')
+        return response.redirect('/')
+      })
+    }
+
   });
   
   router.get('/logout', function (request, response) {
