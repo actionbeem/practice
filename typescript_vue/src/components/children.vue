8d0cbbcf22b375e6b348fbaf5a6042a69b41d41a
김종혁 <actionbeem929@gmail.com>
2019-09-09 17:48:53 +0900

    @Provide/@Inject 데이터 전달

--- typescript_vue/src/components/children.vue
+++ typescript_vue/src/components/children.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
-    <button @click="counter">자식에서 숫자를 증가시킵니다.</button>
+    {{ message }}
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue, Watch, Emit } from 'vue-property-decorator'
+import { Component, Prop, Vue, Watch, Emit, Model, Inject } from 'vue-property-decorator'
 
 @Component
 export default class Children extends Vue {
-  @Emit()
-  counter(){
-    console.log('count')
-  }
+  @Inject() readonly message!: string;
 }
 
 </script>
--- typescript_vue/src/views/Home.vue
+++ typescript_vue/src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <children @counter="counter"></children>
-    <p>
-      부모에서 숫자를 보여줍니다. : {{ count }}
-    </p>
+    <children></children>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Provide } from 'vue-property-decorator';
 import children from '@/components/children.vue'
 
 @Component({
@@ -18,9 +15,7 @@ import children from '@/components/children.vue'
   },
 })
 export default class Home extends Vue {
-  count:number = 0;
-  counter(){
-    this.count++ ;
-  }
+  @Provide('message') msg: string = 'provide/inject example';
+
 }
 </script>
