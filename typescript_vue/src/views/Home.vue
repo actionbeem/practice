96f3b4263e093a504785e2f59fd7c2fd2ec409e9
김종혁 <actionbeem929@gmail.com>
2019-09-09 17:15:55 +0900

    @Watch로 데이터 변화 감지하기

--- typescript_vue/src/views/Home.vue
+++ typescript_vue/src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <message></message>
     <children :parentMessage="message"></children>
+    <button @click="changeMessage">메세지를 바꿉니다.</button>
   </div>
 </template>
 
@@ -19,5 +20,9 @@ import children from '@/components/children.vue'
 })
 export default class Home extends Vue {
   message: string = 'hello world!!';
+
+  changeMessage(){
+    this.message = 'change'
+  }
 }
 </script>
