6dabc205c321dd46650f06a0323851941dab17af
김종혁 <actionbeem929@gmail.com>
2019-09-16 18:55:37 +0900

    todolist with typescript

---
+++ typescript_vue/example/todolist/src/components/item-input.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="form-group">
+      <input 
+        type="text" 
+        class="form-control"
+        @keyup.enter="addItem" 
+        placeholder="할일을 입력하세요">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class ItemInput extends Vue {
+  addItem(){
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ typescript_vue/example/todolist/src/components/item.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" aria-label="..." @change="changeStatus">
+      </span>
+      <input type="text" class="form-control" aria-label="...">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button" @clic="removeItem">X</button>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Item extends Vue {
+  changeStatus(){
+
+  }
+
+  removeItem(){
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
