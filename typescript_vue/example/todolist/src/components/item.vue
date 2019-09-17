726a7dbedadab041c9d8a4c652828685ffd5ed3e
김종혁 <actionbeem929@gmail.com>
2019-09-17 18:31:08 +0900

    라우터 구조 셋팅

--- typescript_vue/example/todolist/src/components/item.vue
+++ typescript_vue/example/todolist/src/components/item.vue
@@ -2,21 +2,25 @@
   <div>
     <div class="input-group">
       <span class="input-group-addon">
-        <input type="checkbox" aria-label="..." @change="changeStatus">
+        <input type="checkbox" :checked="status === 'clear'" aria-label="..." @change="changeStatus">
       </span>
-      <input type="text" class="form-control" aria-label="...">
+      <input type="text" class="form-control" :value="title">
       <span class="input-group-btn">
-        <button class="btn btn-default" type="button" @clic="removeItem">X</button>
+        <button class="btn btn-default" type="button" @click="removeItem">X</button>
       </span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 
 @Component
 export default class Item extends Vue {
+  @Prop() readonly id!: string;
+  @Prop() readonly title!: string;
+  @Prop() readonly status!: 'active' | 'clear';
+
   changeStatus(){
 
   }
